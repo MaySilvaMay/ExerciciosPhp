c08a15f3f44d65adf01ef9e42acd2fdfb685e48c
Mayara <mayara.silva.may@gmail.com>
2019-08-04 14:40:01 -0300

    Solução do desafio

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -11,7 +11,44 @@ class Resolucao implements TextWrapInterface {
    * {@inheritdoc}
    */
   public function textWrap(string $text, int $length): array {
-    return [""];
+	$meuarray = [""];
+	$palavra = "";
+	
+	if (strlen($text)>$length)
+		{
+		for ($i=0; $i<strlen($text);$i++)
+		{
+			if ($text[$i]!=" ")
+			{
+			$palavra = $palavra.$text[$i];
+			}
+			elseif ($text[$i] == " ")
+			{
+				
+				if (strlen($palavra)<=$length)
+				{
+					substr($palavra,0,$length);
+					if (strlen($meuarray[count($meuarray)-1]. " ". $palavra) <= $length)
+					{
+						$meuarray[count($meuarray)-1]=ltrim($meuarray[count($meuarray)-1].(strlen($palavra)==0?"":" "). $palavra);
+					}
+					else
+					{
+						$meuarray[]=$palavra;
+					}
+				}
+				else
+				{
+					substr($palavra,$length,strlen($palavra)-$length);
+				}
+				$palavra="";
+			}   
+		}
+		$meuarray[]=$palavra;
+		}
+		
+	print_r($meuarray);
+	return $meuarray;
   }
-
 }
+
